ed60b0a656ca4781fe0e0dab7d5335c6402a7f68
lilianamanjarres <limavi2007@hotmail.com>
2016-09-14 17:58:26 -0500

    se agrega el archivo ServicioCatalogoMockTest

---
+++ Lab3-MueblesDeLosAlpes/Lab3-MueblesDeLosAlpes-ejb/test/com/losalpes/servicios/ServicioCatalogoMockTest.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.losalpes.servicios;
+
+import com.losalpes.entities.Mueble;
+import java.util.List;
+import javax.ejb.embeddable.EJBContainer;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author lili
+ */
+public class ServicioCatalogoMockTest {
+    
+    public ServicioCatalogoMockTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of agregarMueble method, of class ServicioCatalogoMock.
+     */
+    @Test
+    public void testAgregarMueble() throws Exception {
+        System.out.println("agregarMueble");
+        Mueble mueble = null;
+        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
+        IServicioCatalogoMockLocal instance = (IServicioCatalogoMockLocal)container.getContext().lookup("java:global/classes/ServicioCatalogoMock");
+        instance.agregarMueble(mueble);
+        container.close();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of eliminarMueble method, of class ServicioCatalogoMock.
+     */
+    @Test
+    public void testEliminarMueble() throws Exception {
+        System.out.println("eliminarMueble");
+        long id = 0L;
+        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
+        IServicioCatalogoMockLocal instance = (IServicioCatalogoMockLocal)container.getContext().lookup("java:global/classes/ServicioCatalogoMock");
+        instance.eliminarMueble(id);
+        container.close();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of darMuebles method, of class ServicioCatalogoMock.
+     */
+    @Test
+    public void testDarMuebles() throws Exception {
+        System.out.println("darMuebles");
+        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
+        IServicioCatalogoMockLocal instance = (IServicioCatalogoMockLocal)container.getContext().lookup("java:global/classes/ServicioCatalogoMock");
+        List<Mueble> expResult = null;
+        List<Mueble> result = instance.darMuebles();
+        assertEquals(expResult, result);
+        container.close();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of removerEjemplarMueble method, of class ServicioCatalogoMock.
+     */
+    @Test
+    public void testRemoverEjemplarMueble() throws Exception {
+        System.out.println("removerEjemplarMueble");
+        long id = 0L;
+        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
+        IServicioCatalogoMockLocal instance = (IServicioCatalogoMockLocal)container.getContext().lookup("java:global/classes/ServicioCatalogoMock");
+        instance.removerEjemplarMueble(id);
+        container.close();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
