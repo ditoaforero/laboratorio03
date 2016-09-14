42133f3483153db6c1d67a6d95b8e28f269a757f
lilianamanjarres <limavi2007@hotmail.com>
2016-09-14 16:54:11 -0500

    se implementan los metodos de ServicioCatalogoMock

--- Lab3-MueblesDeLosAlpes/Lab3-MueblesDeLosAlpes-ejb/src/java/com/losalpes/servicios/ServicioCatalogoMock.java
+++ Lab3-MueblesDeLosAlpes/Lab3-MueblesDeLosAlpes-ejb/src/java/com/losalpes/servicios/ServicioCatalogoMock.java
@@ -6,10 +6,13 @@
 package com.losalpes.servicios;
 
 import com.losalpes.entities.Mueble;
+import com.losalpes.entities.Usuario;
+import com.losalpes.excepciones.OperacionInvalidaException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -18,31 +21,37 @@ import javax.ejb.Stateless;
 @Stateless
 public class ServicioCatalogoMock implements IServicioCatalogoMockLocal, IServicioCatalogoMockRemote{
     
-   
     
     @EJB
     private IServicioPersistenciaMockLocal persistencia;
-    
     
     public ServicioCatalogoMock() {
     
     }
 
     @Override
-    public void agregarMueble(Mueble mueble) {
-        boolean found = false;
-        Mueble item;
+    public void agregarMueble(Mueble mueble){
+        try
+        {
+             persistencia.create(mueble);
+        }
+        catch (OperacionInvalidaException ex)
+        {
+           throw new UnsupportedOperationException("Error al agregar Mueble."); 
+        }
     }
     
-    
-    public void agregarItem(Mueble mueble)
-    {
-        
-    }   
-
     @Override
     public void eliminarMueble(long id)  {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try
+        {
+        Mueble m=(Mueble) persistencia.findById(Mueble.class, id);
+        persistencia.delete(m);
+        }
+        catch (OperacionInvalidaException ex)
+        {
+           throw new UnsupportedOperationException("Error al eliminar Mueble."); 
+        }
     }
 
     @Override
@@ -52,7 +61,17 @@ public class ServicioCatalogoMock implements IServicioCatalogoMockLocal, IServic
 
     @Override
     public void removerEjemplarMueble(long id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Mueble m=(Mueble) persistencia.findById(Mueble.class, id);
+        int cantidadActual = m.getCantidad();
+        if(cantidadActual>0){
+            m.setCantidad(cantidadActual-1);
+            persistencia.update(m);
+        }else{
+            JOptionPane.showMessageDialog(null, "No existen mas ejemplares de este mueble");
+            System.out.println("No existen mas ejemplares de este mueble");
+        }
+        
+        
     }
 
     // Add business logic below. (Right-click in editor and choose
